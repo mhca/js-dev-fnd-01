64af795fe0f69abd5773215d82350cc313e53f4c
Heimy <heimy.cruz@gmail.com>
2015-06-18 23:18:41 -0400

    Created Calculator class

---
+++ training/front-end/app/js/src/task-two/calculator.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Heimy on 6/18/2015.
+ */
+
+var Calculator = function () {
+    console.log('instance created');
+    this.memory = 0;
+    this.result = 0;
+    this.operators = '';
+}
